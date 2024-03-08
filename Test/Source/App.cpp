891394f2dd760d649fa96ac7633f2521bad36336
amitansu1024 <amitanshurajneti@gmail.com>
2024-03-08 21:28:54 +0530

    removed test file and added warmup test

--- Test/Source/App.cpp
+++ Test/Source/App.cpp
@@ -1,7 +1,17 @@
-#include "test.h"
 #include "raylib.h"
 
+#include "Linear/Core.h"
+#include "Linear/Matrix.h"
+#include "Linear/Vector.h"
+
+#include "Physics/Vec2.h"
+#include "Physics/PhysicsEngine.h"
+#include "Physics/PhysicsObject.h"
+#include "Physics/Collider.h"
+
 using std::cout, std::endl;
+
+void WarmupTest();
 
 class App {
         PhysicsEngine* m_Engine;
@@ -11,7 +21,6 @@ class App {
             InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TITLE");
             SetTargetFPS(m_Fps);
             m_Engine = PhysicsEngine::GetInstance();
-            m_Engine->SetGravity(0, 2);
         }
 
         void Add(Collider* theCollider) {
@@ -24,9 +33,10 @@ class App {
                 BeginDrawing();
                 DrawFPS(0, 0);
                 ClearBackground(BLACK);
-                for (i32 i = 0; i < m_Engine->GetSize(); i++) {
+                for (size_t i = 1; i < m_Engine->GetSize(); i++) {
                     DrawCircle((int)m_Engine->GetPhysicsObject(i)->GetPosition().GetX(),
-                            (int)m_Engine->GetPhysicsObject(i)->GetPosition().GetY(), 10.0f,
+                            (int)m_Engine->GetPhysicsObject(i)->GetPosition().GetY(),
+                            10.0f,
                             Color{(unsigned char)(10 * (i * 10)), 0, 255, 255});
                 }
                 EndDrawing();
@@ -40,10 +50,12 @@ i32 App::m_Fps = CURR_FPS;
 
 i32 main(void)
 {
-    Collider a(Vec2(4, 0), Vec2(100, 100),  false, true, Shape::Type::Circle);
+    WarmupTest();
+
+    Collider a(Vec2(4, 0), Vec2(100, 100), false, true, Shape::Type::Circle);
     Collider b(Vec2(-1, 1), Vec2(200, 100), false, true, Shape::Type::Circle);
     Collider c(Vec2(-1, 1), Vec2(300, 300), false, true, Shape::Type::Circle);
-    Collider d(Vec2(-1, 1), Vec2(300, 800), false, true, Shape::Type::Circle);
+    Collider d(Vec2(-1, 1), Vec2(300, 400), false, true, Shape::Type::Circle);
     Collider e(Vec2(-1, 1), Vec2(300, 500), false, true, Shape::Type::Circle);
 
     App app;
@@ -63,3 +75,23 @@ i32 main(void)
 
     return 0;
 }
+
+void WarmupTest() {
+    Vector<Vec2> vec;
+    vec.PushBack(Vec2(1, 1));
+    vec.PushBack(Vec2(1, 2));
+    vec.PushBack(Vec2(1, 3));
+
+    vec.Clear();
+    vec.EmplaceBack(1, 2);
+
+
+    Cybug::Matrix<f32> mat(2, 2);
+    mat[0][0] = 1;
+    mat[0][1] = 2;
+    mat[1][0] = 3;
+    mat[1][1] = 4;
+
+    mat.Trace();
+}
+
